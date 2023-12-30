511dfe92cc245eb42e056666a681e0ffc85c29c7
gradro <drozdek.gracjan@gmail.com>
2023-12-30 12:22:49 +0100

    init global constants, nav links

---
+++ constants/index.ts
@@ -0,0 +1,10 @@
+/* App constants */
+
+
+// Navigation menu items
+export const navItems = [
+    { path: "/", label: "Home" },
+    { path: "/discover", label: "Discover" },
+    { path: "/add-place", label: "Add new place" },
+    { path: "/about", label: "About" }
+]
