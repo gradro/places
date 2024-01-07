669724ab6ed3d9a8b51cc87412e7028a0c113622
gradro <drozdek.gracjan@gmail.com>
2024-01-08 00:50:14 +0100

    add auth components

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -4,10 +4,11 @@ import { Button } from "../ui/button";
 import NavLinks from "./Navigation/NavLinks";
 import { CiLogin } from "react-icons/ci";
 import MobileNavigation from "./Navigation/MobileNavigation";
+import SignedIn from "./Auth/SignedIn";
+import SignedOut from "./Auth/SignedOut";
+import UserAvatar from "./Auth/UserAvatar";
 
 const Header = () => {
-
-  const isLoggedIn = false;
 
   return (
     <header className="bg-white border-b pt-2 pb-2">
@@ -22,6 +23,7 @@ const Header = () => {
               alt="Places logo" 
               width={60}
               height={30}
+              loading="eager"
             />
           </Link>
 
@@ -31,29 +33,29 @@ const Header = () => {
           </nav>
 
           <div className="flex gap-4 md:gap-6 items-center">
-            {/* Add new place - button (desktop only) */}
-            <Button className="hidden md:block" asChild variant="default">
-              <Link href="/add-place">Add new place</Link>
-            </Button>
-            {/* User Profile / Login */}
-            {isLoggedIn ? (
-              <Link title="Show my profile" href="/profile">
-                <Image 
-                  src="/assets/uifaces-popular-image.jpg" 
-                  alt="Placeholder Avatar"
-                  width={48}
-                  height={48}
-                  className="rounded-full object-cover w-12 h-12"
-                />
-              </Link>
-            ): (
-              <Link title="Sign in" href="/sign-in">
-                <CiLogin className="w-8 h-8" />
-              </Link>
-            )}
+            <SignedIn>
+              {/* Add new place - button (desktop only) */}
+              <div className="hidden md:block">
+                <Button asChild>
+                  <Link href="/add-place">Add new place</Link>
+                </Button>
+              </div>
+              {/* User Profile / Login */}
+              <UserAvatar dropdown/>
+            </SignedIn>
+           
+            <SignedOut>
+              <Button asChild>
+                <Link title="Sign in" href="/sign-in">
+                  <span className="mr-2">Sign in</span>
+                  <CiLogin className="w-7 h-7" />
+                </Link>
+              </Button>
+            </SignedOut>
 
             {/* Mobile Navigation */}
             <MobileNavigation />
+
           </div>
 
         </div>
